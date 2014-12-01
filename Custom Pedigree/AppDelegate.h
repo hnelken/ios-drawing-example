59f537be5efa0df397e360c5f3802165635a3ee4
Harry Nelken <Harry@Harrys-Mac-Pro.local>
2014-12-01 11:05:52 -0800

    Initial Commit

---
+++ Custom Pedigree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custom Pedigree
+//
+//  Created by Harry Nelken on 12/1/14.
+//  Copyright (c) 2014 Harry Nelken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Custom Pedigree/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Custom Pedigree
+//
+//  Created by Harry Nelken on 12/1/14.
+//  Copyright (c) 2014 Harry Nelken. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
